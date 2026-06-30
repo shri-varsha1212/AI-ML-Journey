06af2f2a65ff34f2b15770831486cedb5a813db2
Shri Varsha <shrivarsha1212@gmail.com>
2026-06-30 19:41:10 +0530

    Create variables.py

---
+++ 01_Python/variables.py
@@ -0,0 +1,21 @@
+# To save a particular result somewhere to work with it later.
+# for example:
+test_var= 2*3
+print(test_var)
+# Here the variable is assigned and when the variable is called it fetches the information where we stored it.
+# To assign a variable one msut not use spaces, only letters, numbers and underscores are to be used.
+# must start with letter or underscore and not number
+
+#Manipulating variables
+# we can always change the assigned value of a variable by overriding the previous value.
+a=2*3
+print(a)
+a=9*2
+print(a)
+# in this condition initially the output will be 6 which is over ridden by assigning the same variable a different value. Therefore the output now will be 18
+# variable defined in a code cell----> all code cells have access to the variable
+# the variable can be used in operations too
+a=a+3
+print(a)
+# here the output will be 21 as it takes the value of a as 18
+#multiple variables can be used in a single code but fetching the exact varibale should be kept in mind.
