c4b25de74f9ac05d89c3150a7b0ff03a361535f0
Shri Varsha <shrivarsha1212@gmail.com>
2026-06-30 19:45:22 +0530

    Update basics.py

--- 01_Python/basics.py
+++ 01_Python/basics.py
@@ -1,4 +1,11 @@
-# Variables and Data Types
+# the pound symbol is used to make a comment line which is not considered by the computer when a program is executed
+# basic functions
+#python uses PEMDAS rule for operations
+print(((1+3)*(9-2)/2**2)
+# according to the above stated rule the output will be 196.0
+# ** this sign is used as exponent function
+print("Hello world!!")
+# print command prints the output directly
 
 name = "Varsha"
 age = 20
